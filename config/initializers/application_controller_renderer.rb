6f2a7a0a3985681d365077c2a99cbdf499a48fa3
takanomiho <olioi.m0nol@gmail.com>
2019-08-25 19:39:58 +0900

    [Finish]5-9

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'bb8b9cf05f276ba7bd5d50df4740857855d2f48ceeeb5b8cb86a183c0d38c6082f8c6602391749968689b7b5cf4c04d5d4635edb959372222ce17d640cbb33c7'
